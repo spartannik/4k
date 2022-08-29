317dd220b84dfa070dd7f9834eb70d1652598fae
spartannik <dead5042@gmail.com>
2022-08-29 20:22:29 +0300

    .

--- 4ksoft/ViewController.swift
+++ 4ksoft/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         initialize()
+
     }
 
+    private func initialize() {
+        view.backgroundColor = UIColor(white: 100, alpha: 1)
+        
+        let label1 = UILabel()
+        label1.text = "Inter"
+        view.addSubview(label1)
+        label1.snp.makeConstraints { maker in
+            maker.left.equalToSuperview().inset(24)
+            maker.top.equalToSuperview().inset(77.3)
+            
+       let label = UILabel()
+            label.text = "QR"
+            view.addSubview(label)
+            label.snp.makeConstraints { maker in
+            maker.left.equalToSuperview().inset(76.75)
+            maker.right.equalTo(label1.snp.right).inset(4.09)
+            maker.top.equalToSuperview().inset(77)
+        }
+    }
 
 }
 
+}
---
+++ Pods/Target Support Files/Pods-4ksoft/Pods-4ksoft-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_4ksoft : NSObject
+@end
+@implementation PodsDummy_Pods_4ksoft
+@end
