a980bb45b62bec5eb1648c525c321edea7695f70
spartannik <dead5042@gmail.com>
2022-09-01 12:05:28 +0300

    fix

--- 4ksoft/ViewController.swift
+++ 4ksoft/ViewController.swift
@@ -9,48 +9,97 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-         initialize()
-
-    }
-
-    private func initialize() {
-        view.backgroundColor = UIColor(white: 100, alpha: 1)
-        
-        let label1 = UILabel()
-        label1.text = "Inter"
-        label1.font = UIFont.systemFont(ofSize: 20)
-        label1.textColor = UIColor(red: 0.655, green: 0.659, blue: 0.667, alpha: 1)
-        view.addSubview(label1)
-        label1.snp.makeConstraints { maker in
-            maker.left.equalToSuperview().offset(24)
-            maker.top.equalToSuperview().offset(77.3)
-            
-       let label = UILabel()
-            label.text = "QR"
-            label.font = UIFont.systemFont(ofSize: 20)
-            label.textColor = UIColor(red: 0, green: 0.561, blue: 0.827, alpha: 1)
-            view.addSubview(label)
-            label.snp.makeConstraints { maker in
-                maker.top.equalToSuperview().offset(77.3)
-                maker.left.equalTo(label1.snp.right).offset(4)
-                
-      let label2 = UILabel()
-          label2.text = "Welcome"
-          label2.font = UIFont.boldSystemFont(ofSize: 35)
-          label2.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-          view.addSubview(label2)
-          label2.snp.makeConstraints { make in
-              maker.top.equalTo(label).offset(450)
-              maker.left.equalTo(label).inset(24)
-                    
-                }
+        setupSubviews()
+        setConstraints()
         }
+
+    //  Labels
+    
+    private lazy var welcomeLabel: UILabel = {
+        let welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome"
+        welcomeLabel.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
+        welcomeLabel.sizeToFit()
+        welcomeLabel.font = .boldSystemFont(ofSize: 35)
+        return welcomeLabel
+    } ()
+    
+    private lazy var myDoors: UILabel = {
+        let myDoors = UILabel()
+        myDoors.text = "My doors"
+        myDoors.textColor = UIColor(red: 0.2, green: 0.22, blue: 0.33, alpha: 1)
+        myDoors.sizeToFit()
+        myDoors.font = .boldSystemFont(ofSize: 20)
+        return myDoors
+    } ()
+    
+    //images
+    
+    private lazy var image1: UIImageView = {
+        let image1 = UIImageView()
+        image1.image = UIImage(named: "Image-1")
+        image1.contentMode = .scaleAspectFit
+        return image1
+    }()
+    
+    private lazy var image2: UIImageView = {
+        let image2 = UIImageView()
+        image2.image = UIImage(named: "Image-2")
+        image2.contentMode = .scaleAspectFit
+        return image2
+    }()
+    
+    private lazy var image3: UIImageView = {
+        let image3 = UIImageView()
+        image3.image = UIImage(named: "Image-3")
+        image3.contentMode = .bottom
+        return image3
+    }()
+
+    private func setConstraints() {
+        image1.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(77)
+            make.left.equalToSuperview().inset(24)
+            make.right.equalToSuperview().inset(265)
+        }
+        
+        image3.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(63)
+            make.left.equalToSuperview().inset(303)
+            make.right.equalToSuperview().inset(27)
+        }
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(250)
+            make.left.equalToSuperview().inset(24)
+            make.right.equalToSuperview().inset(203)
+        }
+        
+        image2.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(108)
+            make.left.equalToSuperview().inset(190)
+            make.right.equalToSuperview().inset(4)
+        }
+        
+        myDoors.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(400)
+            make.left.equalToSuperview().inset(25)
+            make.right.equalToSuperview().inset(265)
+        }
+        
     }
-
+    
+    private func setupSubviews() {
+        view.addSubview(image1)
+        view.addSubview(image3)
+        view.addSubview(welcomeLabel)
+        view.addSubview(image2)
+        view.addSubview(myDoors)
+    }
+    
 }
-
-}
-
