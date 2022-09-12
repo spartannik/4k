5c5480e53d3f40333613e0e6e0889c9cf15208b4
spartannik <dead5042@gmail.com>
2022-09-12 19:36:39 +0300

    fix

---
+++ 4ksoft/CustomTableViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  CustomTableViewCell.swift
+//  4ksoft
+//
+//  Created by Nikita Yashchenko on 11.09.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    static let identifier = "CustomTableViewCell"
+    
+    private lazy var button1: UIButton = {
+        let button1 = UIButton()
+        button1.backgroundColor = .clear
+        button1.layer.cornerRadius = 12
+        button1.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button1
+    }()
+    
+    
+    
+    private let myLabel: UILabel = {
+        let label = UILabel()
+        label.text = """
+Front door
+Home
+"""
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    private let home: UILabel = {
+        let label = UILabel()
+        label.text = "Home"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let statusImage: UIImageView = {
+        let statusImage = UIImageView()
+        statusImage.image = UIImage(named: "statusDoor")
+        statusImage.contentMode = .scaleAspectFit
+        statusImage.clipsToBounds = true
+        return statusImage
+    }()
+    
+    private let lockedDoor: UIImageView = {
+        let statusImage = UIImageView()
+        statusImage.image = UIImage(named: "lockedDoor")
+        statusImage.contentMode = .scaleAspectFit
+        statusImage.clipsToBounds = true
+        return statusImage
+    }()
+    
+    @objc func buttonTapped(sender: UIButton) {
+
+        sender.isSelected = !sender.isSelected
+
+        if sender.isSelected {
+            sender.setImage(UIImage(named:"Locked"), for: .normal)
+        } else {
+            sender.setImage(UIImage(named: "Unlocked"), for: .normal)
+        }
+
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .clear
+        contentView.addSubview(button1)
+        contentView.addSubview(myLabel)
+        contentView.addSubview(statusImage)
+        contentView.addSubview(home)
+        contentView.addSubview(lockedDoor)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let imageSize = contentView.frame.size.height-55
+       
+        myLabel.frame = CGRect(x: 75,
+                               y: 5,
+                               width: contentView.frame.size.width/2 ,
+                               height: contentView.frame.size.height/2)
+        
+        statusImage.frame = CGRect(x: contentView.frame.size.width-imageSize, y: 20, width: imageSize, height: imageSize)
+        
+        lockedDoor.frame = CGRect(x: 25, y: 20, width: 35, height: 35)
+        
+        home.frame = CGRect(x: 75,
+                            y: 25,
+                            width: contentView.frame.size.width/2 ,
+                            height: contentView.frame.size.height/2)
+        
+    }
+    
+}
--- 4ksoft/ViewController.swift
+++ 4ksoft/ViewController.swift
@@ -8,26 +8,33 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath)
+        return cell
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupSubviews()
-        setConstraints()
-        }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+
     //  Labels
-    
+
     private lazy var welcomeLabel: UILabel = {
         let welcomeLabel = UILabel()
         welcomeLabel.text = "Welcome"
-        welcomeLabel.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
+        welcomeLabel.textColor = .black //.init(hexString: "")
         welcomeLabel.sizeToFit()
         welcomeLabel.font = .boldSystemFont(ofSize: 35)
         return welcomeLabel
     } ()
-    
+
     private lazy var myDoors: UILabel = {
         let myDoors = UILabel()
         myDoors.text = "My doors"
@@ -36,30 +43,29 @@ class ViewController: UIViewController {
         myDoors.font = .boldSystemFont(ofSize: 20)
         return myDoors
     } ()
-    
+
     //images
-    
-    private lazy var image1: UIImageView = {
+
+    private lazy var inerQRimage: UIImageView = {
         let image1 = UIImageView()
-        image1.image = UIImage(named: "Image-1")
+        image1.image = UIImage(named: "InterQR")
         image1.contentMode = .scaleAspectFit
         return image1
     }()
-    
-    private lazy var image2: UIImageView = {
+
+    private lazy var home: UIImageView = {
         let image2 = UIImageView()
-        image2.image = UIImage(named: "Image-2")
+        image2.image = UIImage(named: "Home")
         image2.contentMode = .scaleAspectFit
         return image2
     }()
-    
-    private lazy var image3: UIImageView = {
+
+    private lazy var shester: UIImageView = {
         let image3 = UIImageView()
-        image3.image = UIImage(named: "Image-3")
-        image3.contentMode = .bottom
+        image3.image = UIImage(named: "Shester")
         return image3
     }()
-    
+
     private lazy var view1: UIView = {
         let view1 = UIView()
         view1.layer.cornerRadius = 20
@@ -67,14 +73,14 @@ class ViewController: UIViewController {
         view1.contentMode = .scaleAspectFit
         return view1
     }()
-    
+
     private lazy var image4: UIImageView = {
         let image4 = UIImageView()
         image4.image = UIImage(named: "Locked")
         image4.contentMode = .scaleAspectFill
         return image4
     }()
-    
+
     private lazy var image5: UIImageView = {
         let image5 = UIImageView()
         image5.image = UIImage(named: "Unlocked")
@@ -82,112 +88,115 @@ class ViewController: UIViewController {
         return image5
     }()
     
-    private lazy var button1: UIButton = {
-        let button1 = UIButton()
-        button1.backgroundColor = .clear
-        button1.layer.cornerRadius = 12
-        button1.contentMode = .scaleAspectFill
-        button1.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        return button1
-        }()
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(CustomTableViewCell.self,
+                           forCellReuseIdentifier: CustomTableViewCell.identifier)
+        return tableView
+    }()
     
-    
-    @objc func buttonTapped(sender: UIButton) {
-        if sender.currentImage == UIImage(named: "Locked") {
-            sender.setImage(UIImage(named: "Unlocked"), for: .normal)
-        }  else {
-            sender.setImage( UIImage(named:"Locked"), for: .normal)
-        }
-        activityIndicator.startAnimating()
-    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
-       // Code you want to be delayed
-        self.activityIndicator.stopAnimating()
-    }
-}
-    
-    // activity indicator
-    
-    
-    private lazy var activityIndicator: UIActivityIndicatorView = {
-            let activityIndicator = UIActivityIndicatorView()
-            activityIndicator.color = .black
-            return activityIndicator
-        }()
     
 
+//    func delayfunc() {
+//        activityIndicator.startAnimating()
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { // Change `2.0` to the desired number of seconds.
+            // Code you want to be delayed
+//            self.activityIndicator.stopAnimating()
+//        }
+//    }
+
+    // activity indicator
+
+
+//    private lazy var activityIndicator: UIActivityIndicatorView = {
+//        let activityIndicator = UIActivityIndicatorView()
+//        activityIndicator.color = .black
+//        return activityIndicator
+//    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubviews()
+        setConstraints()
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
     private func setConstraints() {
-        image1.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(77)
-            make.left.equalToSuperview().inset(24)
-            make.right.equalToSuperview().inset(265)
+
+        inerQRimage.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(80)
+            make.left.equalToSuperview().inset(25)
         }
-        
-        image3.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(63)
-            make.left.equalToSuperview().inset(303)
-            make.right.equalToSuperview().inset(27)
+
+        shester.snp.makeConstraints { make in
+            make.right.equalToSuperview().inset(25)
+            make.centerY.equalTo(inerQRimage)
         }
-        
+
         welcomeLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(250)
+            make.top.equalToSuperview().inset(157)
             make.left.equalToSuperview().inset(24)
-            make.right.equalToSuperview().inset(203)
         }
-        
-        image2.snp.makeConstraints { make in
+
+        home.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(108)
             make.left.equalToSuperview().inset(190)
             make.right.equalToSuperview().inset(4)
         }
-        
+
         myDoors.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(400)
+            make.top.equalToSuperview().inset(307)
             make.left.equalToSuperview().inset(25)
             make.right.equalToSuperview().inset(265)
         }
+
+//        image4.snp.makeConstraints { make in
+//            make.top.equalToSuperview().inset(478)
+//            make.left.equalToSuperview().inset(16)
+//            make.right.equalToSuperview().inset(16)
+//            make.width.equalTo(50)
+//            make.height.equalTo(50)
+//        }
+
+//        image5.snp.makeConstraints { make in
+//            make.top.equalToSuperview().inset(478)
+//            make.left.equalToSuperview().inset(16)
+//            make.right.equalToSuperview().inset(16)
+//            make.width.height.equalTo(image4)
+//       }
+
+//        activityIndicator.snp.makeConstraints { make in
+//            make.top.equalToSuperview().inset(476)
+//            make.left.equalToSuperview().inset(16)
+//            make.right.equalToSuperview().inset(16)
+//            make.width.equalTo(50)
+//            make.height.equalTo(50)
+//        }
         
-        button1.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(442.5)
+        tableView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(360)
             make.left.equalToSuperview().inset(16)
             make.right.equalToSuperview().inset(16)
-            make.width.equalTo(344)
-            make.height.equalTo(120)
+            make.width.equalTo(300)
+            make.height.equalTo(400)
         }
         
-        image4.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(478)
-            make.left.equalToSuperview().inset(16)
-            make.right.equalToSuperview().inset(16)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        }
-        
-        image5.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(478)
-            make.left.equalToSuperview().inset(16)
-            make.right.equalToSuperview().inset(16)
-            make.width.height.equalTo(image4)
-        }
-        
-        activityIndicator.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(476)
-            make.left.equalToSuperview().inset(16)
-            make.right.equalToSuperview().inset(16)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        }
     }
-    
+
     private func setupSubviews() {
-        view.addSubview(image1)
-        view.addSubview(image3)
+        view.addSubview(inerQRimage)
+        view.addSubview(shester)
         view.addSubview(welcomeLabel)
-        view.addSubview(image2)
+        view.addSubview(home)
         view.addSubview(myDoors)
-        view.addSubview(image4)
-        view.addSubview(image5)
-        view.addSubview(button1)
-        view.addSubview(activityIndicator)
+//        view.addSubview(image4)
+//        view.addSubview(image5)
+//        view.addSubview(button1)
+//        view.addSubview(activityIndicator)
+        view.addSubview(tableView)
     }
-    
+
 }
+
